741af31e6cb3c0468e5ae26ec7a9df66b32ff6fa
zenix-s <sergiofs19@gmail.com>
2024-04-10 12:31:59 +0200

    servicio delete workspace
    Se ha montado el endpoint para poder eliminar workspaces

--- back/src/controller/workspace/controllerWorkspace.ts
+++ back/src/controller/workspace/controllerWorkspace.ts
@@ -1,4 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import deleteWorkspace from "@/services/workspace/deleteWorkspace";
+
+// /workspace/:idWorkspace
+
 export function deleteWorkspaceController(req: Request, res: Response) {
-		res.send("Eliminar workspace");
+	const idWorkspace:number = parseInt(req.params.idWorkspace);
+  const deleted:boolean = deleteWorkspace(idWorkspace)
+	res.json(
+		{
+			"deleted":deleted,
+			"message":"workspace eliminado"
+		}
+	);
 }
---
+++ back/src/services/workspace/deleteWorkspace.ts
@@ -0,0 +1,6 @@
+
+function deleteWorkspace(idWorkspace:number):boolean{
+	return true;
+}
+
+export default deleteWorkspace;
