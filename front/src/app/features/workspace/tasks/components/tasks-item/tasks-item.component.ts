8f0e1a0dea7478953535c2647225f7a4cb6d34a9
zenix-s <sergiofs19@gmail.com>
2024-04-12 14:47:02 +0200

    Add TasksItemComponent template and update TasksItemComponent in tasks module

--- front/src/app/features/workspace/tasks/components/tasks-item/tasks-item.component.ts
+++ front/src/app/features/workspace/tasks/components/tasks-item/tasks-item.component.ts
@@ -3,11 +3,7 @@ import { Task } from '@app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tasks-item',
-  template: `
-    <div>
-      {{ task.id }} - {{ task.name }} - {{ task.deadline | date : 'yyyy-MM-dd' }} - {{ task.completed }}
-    </div>
-  `,
+  templateUrl: './tasks-item.component.html',
   styles: ``,
 })
 export class TasksItemComponent {
