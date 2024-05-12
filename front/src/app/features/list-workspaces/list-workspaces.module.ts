f10a93aafb47c06a72d2f5384d97caab86a9fe1e
zenix-s <sergiofs19@gmail.com>
2024-05-12 16:37:53 +0200

    Refactor list-workspaces component and module

--- front/src/app/features/list-workspaces/list-workspaces.module.ts
+++ front/src/app/features/list-workspaces/list-workspaces.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ListWorkspacePageComponent } from './list-workspace-page/list-workspace-page.component';
+import { WorkspaceItemComponent } from './components/workspace-item/workspace-item.component';
 
 
 @NgModule({
   declarations: [
     ListWorkspacePageComponent,
+    WorkspaceItemComponent,
   ],
   imports: [
     CommonModule,
